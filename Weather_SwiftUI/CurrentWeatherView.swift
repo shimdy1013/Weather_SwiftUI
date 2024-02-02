7d22eec0cfc063f5d91fe402442ad5c3afa65713
shimdy1013 <shimdy1013@naver.com>
2024-02-03 00:49:36 +0900

    CurrentWeatherView 기본 틀 완성

---
+++ Weather_SwiftUI/CurrentWeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  CurrentWeatherView.swift
+//  Weather_SwiftUI
+//
+//  Created by 심두용 on 2024/02/02.
+//
+
+import SwiftUI
+
+struct CurrentWeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CurrentWeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrentWeatherView()
+    }
+}
